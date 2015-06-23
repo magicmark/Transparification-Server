b26da8418177b0441281daeef760fe6c0df139dd
Mark <mark@larah.me>
2015-06-23 23:53:10 +0100

    new structure

--- information.js
+++ information.js
@@ -1,5 +1,49 @@
 'use strict';
 
+var hosts = {
+  'bbc.co.uk': 'bbc',
+  'm.bbc.co.uk': 'bbc',
+  'reddit.com': 'reddit',
+  'condenast.co.uk': 'condenast'
+};
+
+var organisations = {
+  'bbc': {
+    'owner': null,
+    'info': 'British Broadcasting Corporation'
+  },
+  'reddit': {
+    'owner': 'condenast',
+    'info': 'Reddit'
+  },
+  'condenast': {
+    'owner': 'advancepublications',
+    'info': 'Condé Nast'
+  },
+  'advancepublications': {
+    'owner': null,
+    'info': 'Advance Publications'
+  }
+};
+
+function getInfoString (organisationKey) {
+  var infoString = '';
+  for (;;) {
+    var organisation = organisations[organisationKey];
+    console.log(organisation);
+    if (!organisation) { return infoString; }
+    if (infoString.length) {
+      infoString += " -> ";
+    }
+    infoString += organisation.info;
+    organisationKey = organisation.owner; 
+  }
+};
+
 module.exports = {
-  'bbc.co.uk': 'British Broadcasting Corporation'
+  getInfo: function (host) {
+    console.log(host);
+    var siteKey = hosts[host];
+    return getInfoString(siteKey);
+  }
 };
--- routes.js
+++ routes.js
@@ -5,8 +5,8 @@ module.exports = function(app) {
 
 
   app.get('/info/:site', function(req, res) {
-    
-    var site = information[req.params.site];
+    console.log(req.get('API-Version'));
+    var site = information.getInfo(req.params.site);
     res.json({
       'success' : (!!site),
       'info': site
